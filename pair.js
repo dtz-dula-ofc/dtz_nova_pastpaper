60b37c0b17db7fabdf1957288d0b935c94362cca
dtz-dula-ofc <dula9x@gmail.com>
2026-02-15 16:05:09 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -21,7 +21,7 @@ const {
     proto,
     prepareWAMessageMedia,
     generateWAMessageFromContent
-} = require('@whiskeysockets/baileys');
+} = require(baileys');
 
 // Global cache for pastpaper search results
 global.ppSearchCache = {};
